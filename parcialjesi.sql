3dc70b9dd29d1136cc6ccd64df5065ddc03c1012
Jesica <47221127+jesicagimenez94@users.noreply.github.com>
2020-11-01 19:34:47 -0300

    Actualizacion base de datos

--- parcialjesi.sql
+++ parcialjesi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-06-2020 a las 17:55:09
+-- Tiempo de generación: 01-11-2020 a las 23:30:04
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.2.29
 
@@ -20,6 +20,50 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `parcialjesi`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `delete_movies`
+--
+
+CREATE TABLE `delete_movies` (
+  `id_movie` int(11) NOT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `anio` int(11) DEFAULT NULL,
+  `puntaje` float DEFAULT NULL,
+  `duracion` int(11) DEFAULT NULL,
+  `genero` varchar(100) DEFAULT NULL,
+  `decripcion` varchar(250) DEFAULT NULL,
+  `imagen` varchar(250) DEFAULT NULL,
+  `fecha_eliminacion` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `delete_movies`
+--
+
+INSERT INTO `delete_movies` (`id_movie`, `titulo`, `anio`, `puntaje`, `duracion`, `genero`, `decripcion`, `imagen`, `fecha_eliminacion`) VALUES
+(26, 'La Cenicienta\r\n', 1950, 8.4, 76, 'Animación', 'Con una malvada madrastra y dos hermanastras celosas que la mantienen esclavizada y en harapos, Cenicienta no tiene oportunidad de asistir al baile real, hasta que aparece su hada madrina.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAjoqKNE_YmiNWGc0BLSQqoV7njqDLrGkd3doTtxiCzpK7818h', '2020-11-01 18:35:55');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial_actualizaciones`
+--
+
+CREATE TABLE `historial_actualizaciones` (
+  `id_movies` int(11) NOT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `fecha_modificacion` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `historial_actualizaciones`
+--
+
+INSERT INTO `historial_actualizaciones` (`id_movies`, `titulo`, `fecha_modificacion`) VALUES
+(27, 'jjjjjjj', '2020-11-01 19:24:28');
 
 -- --------------------------------------------------------
 
@@ -44,8 +88,8 @@ CREATE TABLE `movies` (
 
 INSERT INTO `movies` (`id_movie`, `titulo`, `anio`, `puntaje`, `duracion`, `genero`, `descripcion`, `imagen`) VALUES
 (1, 'X-MEN', 2000, 6.5, 104, 'Accion/Ciencia Ficcion', 'El profesor Xavier lidera a Wolverine y otros superhéroes contra Magneto, un mutante que dominará a la humanidad.', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTW9vPlQrh1oqgltle82NU4pdugEGfo2yL2axMQmNusHloeDP1b'),
-(2, 'X-MEN 2', 2003, 7.4, 134, 'Acción/Ciencia ficción', 'Un mutante desconocido se introduce en la Casa Blanca e intenta asesinar al presidente sin éxito. Charles Xavier y el resto de mutantes tratarán de capturar al magnicida fallido', 'https://vignette.wikia.nocookie.net/xmen9260/images/d/d1/X-Men_2.jpg/revision/latest?cb=20170223150900&path-prefix=es'),
-(3, 'X-Men III - La batalla Final', 2006, 6.7, 104, 'Acción/Ciencia ficción ', 'El descubrimiento de una cura para las mutaciones divide la situación en dos bandos. Los mutantes pueden elegir abandonar sus poderes y convertirse totalmente en humanos o continuar con sus habilidades y seguir aislados. Una guerra entre los seguidor', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRO_AOo86XPZrjGTAgX-7Jnz5y6tH3cNX6-pt3Xd04sD6Zkoq4o'),
+(2, 'X-MEN 2', 2003, 7.4, 134, 'fantasia', 'Un mutante desconocido se introduce en la Casa Blanca e intenta asesinar al presidente sin éxito. Charles Xavier y el resto de mutantes tratarán de capturar al magnicida fallido', 'https://vignette.wikia.nocookie.net/xmen9260/images/d/d1/X-Men_2.jpg/revision/latest?cb=20170223150900&path-prefix=es'),
+(3, 'X-Men III - La batalla Final', 2006, 6.7, 104, 'accion', 'El descubrimiento de una cura para las mutaciones divide la situación en dos bandos. Los mutantes pueden elegir abandonar sus poderes y convertirse totalmente en humanos o continuar con sus habilidades y seguir aislados. Una guerra entre los seguidor', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRO_AOo86XPZrjGTAgX-7Jnz5y6tH3cNX6-pt3Xd04sD6Zkoq4o'),
 (4, 'X-Men Orígenes: Wolverine\r\n', 2009, 6.6, 120, 'Acción/Aventura ', 'Buscando consuelo de su oscuro pasado Logan, más conocido como Wolverine, parece haber encontrado el amor y la alegría con Kayla Silverfox pero todo cambia cuando ella es asesinada.', 'https://i.pinimg.com/originals/87/51/e1/8751e13ccc07cf538ce5a1e94a5c3473.jpg'),
 (5, 'X-Men: primera generación', 2011, 6.8, 132, 'Acción/Ciencia ficción', 'A principios de los años 60, Charles Xavier y Magneto se convierten en grandes amigos. Usando sus poderes, se unen para proteger al mundo de una guerra nuclear.', 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS8RduB1InjRmhLthupqByGBWPA_XTKHOzRyyuCNb_2lF2OxfMw'),
 (6, 'X-Men: Días del futuro pasado\r\n', 2014, 6.9, 151, 'Acción/Ciencia ficción', 'Los X-Men unen fuerzas con sus versiones más jóvenes con el fin de cambiar el pasado y salvar su futuro y el de la humanidad.', 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTvoqkmbzJlG1zYnal137C5NAmueaNzzuqWZECfjZ4WQMCHIEeD'),
@@ -63,12 +107,28 @@ INSERT INTO `movies` (`id_movie`, `titulo`, `anio`, `puntaje`, `duracion`, `gene
 (18, 'Pesadilla en Elm Street 3: Dream Warriors', 1987, 6.6, 96, 'Terror', 'Jóvenes que comparten pesadillas similares en una institución se unen para librarse de la influencia de Freddy Krueger.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVCimEGGgUtS1kD4b1tU_fOgjgF0U8BGqqWqly0nfgv5su7X17'),
 (19, 'Pesadilla en Elm Street 4: The Dream Master', 1988, 5.7, 159, 'Terror', 'Una adolescente usa el poder de sus amigos difuntos para terminar con el reinado de terror de Freddy Krueger.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS37qhHyoeu_Oge0UVEsnv3KV7JP3wLj3Dy7JeulEFDXC-rLVDA'),
 (20, 'Freddy vs. Jason', 2003, 5.7, 98, 'Terror', 'Unos adolescentes quedan atrapados en medio de la batalla entre Freddy Krueger y el asesino enmascarado Jason.', 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRlQqm2YUCdxP63Ww2q3jDfjeoqZlskzictKcJQ7mbXlZXvziLr'),
-(21, 'Alien vs. Predator\r\n', 2004, 7, 121, 'Terror', 'Dos razas alienígenas se enfrentan a muerte en una pirámide enterrada bajo el hielo de la Antártida. Poco importa quién gane, perderá la humanidad.', 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR4HOjBsD7a7vuBfGHIW0gBn8FRXN6AvXZ0k43DXipbxL4QQCxd'),
 (22, 'Mulan\r\n', 1998, 7.6, 86, 'Animación', 'El ejército de los Hunos, encabezado por el malvado Shun Yiu, quiere conquistar China. El emperador, para impedírselo, ha mandado a filas a todos los varones con el fin de proteger el imperio. Por otra parte, Mulán es una chica joven y valiente que v', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRooYm0BOo70nQWVFnY-QpAyU4u7lt7wboWqR7bry-t0cTceyfM'),
-(23, 'La Sirenita', 1989, 6.8, 86, 'Animación', 'La sirena Ariel está fascinada por el mundo de los humanos, pero su padre le prohíbe relacionarse con ellos. En un viaje secreto, se enamora de un humano y recurre a una perversa hechicera para que, mediante un conjuro, su amor triunfe.', 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRWOOC0-qyIRBM_wHvbh7MYxebNWQaWEfniJNfsSixdqBHhsBjo'),
 (24, 'Pocahontas\r\n', 1995, 7.7, 86, 'Animación', 'Pocahontas es la hija de Powhatan, el jefe de una tribu india de América del Norte. Un día observa la llegada de un grupo de colones ingleses, encabezados por el ambicioso gobernador Radcliff y el valiente capitán John Smith. Acompañada de sus compañ', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTK1JI-NyXU4fM4pUjHumTAa4klkwN6DCjvKHAPlooJsa3fMJAZ'),
 (25, 'Aladdín', 1992, 7.4, 85, 'Animación', 'En Bagdad, un ladrón árabe halla una lámpara mágica y trata de ganar el amor de una princesa con la ayuda de los deseos concedidos por un genio.', 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSLJfyLL9_3p3S8SSgLr8KFLfP0Z8o40ejGkHp8az08pTTUegMH'),
-(26, 'La Cenicienta\r\n', 1950, 8.4, 76, 'Animación', 'Con una malvada madrastra y dos hermanastras celosas que la mantienen esclavizada y en harapos, Cenicienta no tiene oportunidad de asistir al baile real, hasta que aparece su hada madrina.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAjoqKNE_YmiNWGc0BLSQqoV7njqDLrGkd3doTtxiCzpK7818h');
+(27, 'jjjjjjj', 202, 8.2, 300, 'accion', 'jjjjjjjj', 'kkkkkkkk');
+
+--
+-- Disparadores `movies`
+--
+DELIMITER $$
+CREATE TRIGGER `DIPARADOR_ACTUALIZAR_PELICULAS` BEFORE UPDATE ON `movies` FOR EACH ROW BEGIN
+insert into historial_actualizaciones(id_movies,titulo,fecha_modificacion)
+values (OLD.id_movie,NEW.titulo,now()) ;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `DIPARADOR_ELIMINAR_PELICULAS` AFTER DELETE ON `movies` FOR EACH ROW BEGIN
+insert into delete_movies(id_movie,titulo,anio,puntaje,duracion,genero,decripcion,imagen,fecha_eliminacion)
+values (old.id_movie,old.titulo,old.anio,old.puntaje,old.duracion,old.genero,old.descripcion,old.imagen,now());
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -89,11 +149,24 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id_usuario`, `nombre`, `email`, `contrasenia`) VALUES
 (1, 'admin', 'admin@gmail.com', 'admin1234'),
-(6, 'Jesica', 'jesicagimenez2294@gmail.com', 'jesi1234');
+(6, 'Jesica', 'jesicagimenez2294@gmail.com', 'jesi1234'),
+(7, 'ana', 'nan', '123456');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `delete_movies`
+--
+ALTER TABLE `delete_movies`
+  ADD PRIMARY KEY (`id_movie`);
+
+--
+-- Indices de la tabla `historial_actualizaciones`
+--
+ALTER TABLE `historial_actualizaciones`
+  ADD PRIMARY KEY (`id_movies`);
 
 --
 -- Indices de la tabla `movies`
@@ -115,13 +188,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `movies`
 --
 ALTER TABLE `movies`
-  MODIFY `id_movie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id_movie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
