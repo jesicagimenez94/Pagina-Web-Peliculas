05c5da3cdc89f2ebb1e15cbfd5343f926602822f
Jesica <47221127+jesicagimenez94@users.noreply.github.com>
2020-11-26 18:34:00 -0300

    actualizacion txt

--- peliculas/pdf.php
+++ peliculas/pdf.php
@@ -2,19 +2,21 @@
 include 'conexion.php';
 
 if(isset($_POST["pdf"])){
-$response='hola mundo';
+    $fh = fopen('json/result.pdf', 'w'); 
 
-    $fp= fopen('json/result.PDF','w');
-    fwrite($fp, $response);
-    
-    
-    fclose($fp);
-
-    
+    $result = mysqli_query($conexion,"select * from movies"); 
+    while ($row = mysqli_fetch_array($result)) {   
+     $last = end($row);   
+     $num = mysqli_num_fields($result) ;  
+     for($i = 0; $i < $num; $i++) {    
+      fwrite($fh, $row[$i]);      
+      if ($row[$i] != $last) 
+       fwrite($fh, " \n"); 
+     }                 
+     fwrite($fh, "\n  --------------------------------------------------------------------------------------------  \n"); 
+} 
+    fclose($fh); 
 }
-//var_dump($response);
-//http://localhost/parcial/Pagina-Web-Peliculas/peliculas/json.php
-//die (json_encode($response));
 ?>
     
 
--- peliculas/txt.php
+++ peliculas/txt.php
@@ -1,28 +1,21 @@
 <?php
 include 'conexion.php';
 
-if(isset($_POST["txt"])){
-$response=array();
-$u=mysqli_query($conexion,"select * from movies");
-$response['movies']=array();
-foreach ($u as $key){
-    $datos=array();
-    foreach ($key as $k => $v)
-        $datos[$k]=$v;
-    array_push($response['movies'],$datos);
-    $fp= fopen('json/result.txt','w');
-    fwrite($fp, json_encode($response));
-    
-    
-    fclose($fp);
-   /* $salida=$response.".txt";
-    header('Content-type: txt ');
-    header('Content-disposition: attachment; filname=',$salida);*/
-    
-}}
-//var_dump($response);
-//http://localhost/parcial/Pagina-Web-Peliculas/peliculas/json.php
-//die (json_encode($response));
-?>
-    
 
+if(isset($_POST["txt"])){
+    $fh = fopen('json/result.txt', 'w'); 
+
+    $result = mysqli_query($conexion,"select * from movies"); 
+    while ($row = mysqli_fetch_array($result)) {   
+     $last = end($row);   
+     $num = mysqli_num_fields($result) ;  
+     for($i = 0; $i < $num; $i++) {    
+      fwrite($fh, $row[$i]);      
+      if ($row[$i] != $last) 
+       fwrite($fh, " \n"); 
+     }                 
+     fwrite($fh, "\n  --------------------------------------------------------------------------------------------  \n"); 
+} 
+    fclose($fh); 
+}
+?>
